6f1ad4895ee72b0c261bb60c858485028675eed8
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-02-13 12:55:28 +0700

    Lab 18.1 Complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect a,Rect b){
+    double x,y;
+    x = min((a.x+a.w),(b.x+b.w))-max(a.x,b.x);
+    y = min(a.y,b.y)-max((a.y-a.h),(b.y-b.h));
 
-
+    if(x>0&&y>0) return x*y;
+    else return 0;
 
 }
